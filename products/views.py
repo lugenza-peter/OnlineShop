80ead85e7953f9656316a9702f268e33ebffbdbd
luge <lagmaztv@gmail.comgit config --global user.email lagmaztv@gmail.com>
2020-04-08 16:52:01 +0300

    sqlite is just a file (temporary db)

---
+++ products/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return HttpResponse ('hello world')
+
+
+def new(request):
+    return HttpResponse('new products')
