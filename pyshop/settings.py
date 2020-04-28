f79fa19181e47a83cc7abde629317c65e498adf2
lugenza-peter <peterlugenza@gmail.com>
2020-04-29 01:08:13 +0300

    applied settings

--- pyshop/settings.py
+++ pyshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^^ds)ff=$e6i=bewg+ue@&@^+8xi$$@6ch(fk1$cm8lu^&2-xz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
